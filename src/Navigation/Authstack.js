5517b790db263f19a583a1f5fab75cf48030216c
Minal <minal.bhoge1@gmail.com>
2024-03-25 15:39:22 +0530

    update code

--- src/Navigation/Authstack.js
+++ src/Navigation/Authstack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View,Text } from "react-native";
-import { Login ,Register} from "..";
+import { Home, Login ,Register} from "..";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import navigationStrings from "../constant/navigationStrings";
 
@@ -12,6 +12,7 @@ const Authstack = () => {
       <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name={navigationStrings.LOGIN} component={Login} />
         <Stack.Screen name={navigationStrings.REGISTER} component={Register} />
+        <Stack.Screen name={navigationStrings.HOME} component={Home} />
 
       </Stack.Navigator>
   )
--- src/Navigation/Mainstack.js
+++ src/Navigation/Mainstack.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View,Text } from "react-native";
+import { Home, Login ,Register} from "..";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import navigationStrings from "../constant/navigationStrings";
+
+const Stack = createNativeStackNavigator();
+
 
 const Mainstack = () => {
   return (
-    <View>
-      <Text>Mainstack</Text>
-    </View>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name={navigationStrings.HOME} component={Home} />
+
+      </Stack.Navigator>
   )
 }
 
 export default Mainstack
 
-const styles = StyleSheet.create({})
+
